144a18bb791f45dd0b2e92f64ac1aa1a0a1ed040
Hollayemi <[sephanyemmitty@gmail.com]>
2023-03-30 18:18:10 +0100

    my first push on car booking

---
+++ src/pages/auth/createAccount.js
@@ -0,0 +1,146 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { newAccount } from '../../state/slices/auth/Signup';
+import { SelectPicker, toaster, Message } from 'rsuite';
+
+const Signup = () => {
+    const dispatch = useDispatch();
+    const [confPass, setConfPass] = useState();
+    const [formData, setFormData] = useState({
+        name: '',
+        email: '',
+        register_as: null,
+        register_id: '',
+        password: '',
+    });
+    let newValue = {};
+    function updateValue(newVal, variable) {
+        variable === 'name' && (newValue = { name: newVal });
+        variable === 'email' && (newValue = { email: newVal });
+        variable === 'regAs' && (newValue = { register_as: newVal });
+        variable === 'regId' && (newValue = { register_id: newVal });
+        variable === 'password' && (newValue = { password: newVal });
+        setFormData({
+            ...formData,
+            ...newValue,
+        });
+    }
+
+    const RegType = [
+        { value: 'Driver', label: 'Driver' },
+        { value: 'Student', label: 'Student' },
+    ];
+
+    const signupHandler = (e) => {
+        e.preventDefault();
+        if (formData.password === confPass) {
+            newAccount(formData, dispatch);
+        } else {
+            toaster.push(<Message type="error">Password not match</Message>, {
+                placement: 'topCenter',
+            });
+        }
+    };
+
+    const fixedStyle =
+        'border focus:border-red-100 w-full mb-4 bg-transparent rounded-md w-full h-9 px-4 py-1 ';
+
+    return (
+        <div className="website-main-bg-image h-full w-full absolute">
+            <div className="absolute h-full w-full bg-slate-500 opacity-80"></div>
+            <div className="flex items-center justify-center h-full w-full absolute top-0 left-0">
+                <div className="flex items-center border border-slate-200 p-6 mx-1 md:w-[370px] bg-gray-100 rounded justify-center flex-col">
+                    <h1 className="font-bold">AAUA Shuttle Sign up</h1>
+                    <div>
+                        <form className="">
+                            <input
+                                className={fixedStyle}
+                                placeholder="Name"
+                                type="text"
+                                onChange={(e) =>
+                                    updateValue(e.target.value, 'name')
+                                }
+                            />
+
+                            <input
+                                className={fixedStyle}
+                                placeholder="email"
+                                type="text"
+                                onChange={(e) =>
+                                    updateValue(e.target.value, 'email')
+                                }
+                            />
+
+                            <SelectPicker
+                                data={RegType}
+                                className="w-full mb-4"
+                                size="md"
+                                placeholder="Register as"
+                                onChange={(e) => updateValue(e, 'regAs')}
+                            />
+
+                            <input
+                                className={`${fixedStyle} ${
+                                    !formData.register_as && ' hidden'
+                                }`}
+                                placeholder={
+                                    formData.register_as === 'Driver'
+                                        ? 'Enter your car number'
+                                        : 'Enter your matric number'
+                                }
+                                type="text"
+                                onChange={(e) =>
+                                    updateValue(e.target.value, 'regId')
+                                }
+                            />
+
+                            <div className="w-full flex px-2 text-xs justify-end items-center">
+                                <i className="fa fa-eye hover:text-blue-200 text-blue-100"></i>
+                            </div>
+                            <input
+                                className={fixedStyle}
+                                placeholder="password"
+                                type="password"
+                                onChange={(e) =>
+                                    updateValue(e.target.value, 'password')
+                                }
+                            />
+
+                            <div className="w-full flex px-2 text-xs justify-end items-center">
+                                <i className="fa fa-eye hover:text-blue-200 text-blue-100"></i>
+                            </div>
+                            <input
+                                className={fixedStyle}
+                                placeholder="confirm password"
+                                type="password"
+                                onChange={(e) => setConfPass(e.target.value)}
+                            />
+
+                            <button
+                                onClick={(e) => signupHandler(e)}
+                                className="font-bold h-8 w-full bg-blue-500 text-white rounded-md shadow-lg"
+                            >
+                                Sign up
+                            </button>
+
+                            <div className="w-full flex px-2 mt-6 font-bold text-xs justify-between mt-2 items-center">
+                                <p className="text-sm text-blue-500">
+                                    Already have account{' '}
+                                </p>
+                                <Link
+                                    to="/signin"
+                                    className="text-sm cursor-pointer hover:text-blue-700 text-blue-600"
+                                >
+                                    Sign in
+                                </Link>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Signup;
---
+++ src/pages/website/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import BookBus from '../../components/website/bookModal';
+import HomeWrapper from '../../components/website/HomeWrapper';
+import AvailablePickup from './available_pickup';
+const Home = () => {
+    const { userData } = useSelector((state) => state.reducer.loginReducer);
+    const myData = userData && userData.data ? userData.data : null;
+    return (
+        <HomeWrapper userData={myData}>
+            <div className="flex items-center pt-10 md:pt-0 flex-col sm:flex-row justify-evenly absolute top-0 left-0 h-full w-full">
+                <div className="w-full sm:w-3/4 md:w-1/2 px-6 md:px-24">
+                    <h2 className="font-black text-xl sm:text-4xl text-white">
+                        Now <br /> its easy to get Shuttle Bus
+                    </h2>
+                    <p className="text-sm text-white mt-5">
+                        We give car support to all students in school opening
+                        hours. AAUA students can now book a shuttle to convey
+                        one or many to their lecture theaters
+                    </p>
+                </div>
+                <div className="md:w-1/2 md:mt-10">
+                    <BookBus userData={myData} />
+                    {/* <AvailablePickup /> */}
+                </div>
+            </div>
+        </HomeWrapper>
+    );
+};
+
+export default Home;
