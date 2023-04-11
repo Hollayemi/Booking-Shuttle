d213ac0cd9302c5828227ac5a5631315f131fd5b
Stephen Olayemi <stephenyemmitty@gmail.com>
2023-04-11 14:18:37 +0100

    integrated paystack

--- src/state/slices/api/baseApi.js
+++ src/state/slices/api/baseApi.js
@@ -2,5 +2,5 @@ import axios from 'axios';
 // https://shuttle-backend.onrender.com
 // 'http://localhost:4040/aau_shuttle',
 export default axios.create({
-    baseURL: 'http://localhost:4040/aau_shuttle',
+    baseURL: 'https://shuttle-backend.onrender.com/aau_shuttle',
 });
